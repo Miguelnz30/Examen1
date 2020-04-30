2a2798479107e55c6aa94a3f3a0bb366023c2654
Miguelnz30 <miguelcanales72@hotmail.com>
2020-04-29 21:15:32 -0600

    otro

--- src/Hospitales/HospitalNicoya.java
+++ src/Hospitales/HospitalNicoya.java
@@ -1,5 +1,27 @@
 package Hospitales;
 
-public class HospitalNicoya {
+import java.util.ArrayList;
+
+import Component.Hospital;
+import Component.Paciente;
+
+public class HospitalNicoya extends OMS{
+
+	public HospitalNicoya(ArrayList<Paciente> persona) {
+		super(persona);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected boolean isGamHospital() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	Hospital getNombreHospital() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
